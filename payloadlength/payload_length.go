74446ff484031a020fd584d2c347eac0de30170f
rj <wangrj1981@gmail.com>
2020-01-13 17:38:12 +0800

    Add payload length for 16bits

--- payloadlength/payload_length.go
+++ payloadlength/payload_length.go
@@ -24,6 +24,57 @@ const (
     mask = uint8(3 << 6)
 )
 
+func CountBytes16(length uint16) (int, error) {
+    switch {
+    case length <= max8Bits:
+        return 1, nil
+    case length <= max16Bits:
+        return 2, nil
+    }
+    return 0, ErrPayloadLengthTooLarge
+}
+
+func Encode16(length uint16) (buf []byte, nbytes int, err error) {
+    switch {
+    case length <= max8Bits:
+        nbytes = 1
+        buf = append(buf, byte(length))
+    case length <= max16Bits:
+        nbytes = 2
+        buf = make([]byte, nbytes)
+        binary.BigEndian.PutUint16(buf, uint16(length))
+        buf[0] |= mark16Bits
+    default:
+        err = ErrPayloadLengthTooLarge
+    }
+    return
+}
+
+func Decode16(buf []byte) (length uint16, nbytes int, err error) {
+    if len(buf) == 0 {
+        err = ErrMalformedPayloadLength
+        return
+    }
+    switch buf[0] & mask {
+    case mark8Bits:
+        nbytes = 1
+        length = uint16(buf[0])
+    case mark16Bits:
+        nbytes = 2
+        if len(buf) < nbytes {
+            err = ErrMalformedPayloadLength
+            return
+        }
+        unmarkedbuf := append([]byte{}, buf...)
+        unmarkedbuf[0] &= ^uint8(mark16Bits)
+        length = uint16(binary.BigEndian.Uint16(unmarkedbuf))
+    default:
+        err = ErrMalformedPayloadLength
+        return
+    }
+    return
+}
+
 func CountBytes64(length uint64) (int, error) {
     switch {
     case length <= max8Bits:
@@ -79,24 +130,27 @@ func Decode64(buf []byte) (length uint64, nbytes int, err error) {
             err = ErrMalformedPayloadLength
             return
         }
-        buf[0] &= ^uint8(mark16Bits)
-        length = uint64(binary.BigEndian.Uint16(buf))
+        unmarkedbuf := append([]byte{}, buf...)
+        unmarkedbuf[0] &= ^uint8(mark16Bits)
+        length = uint64(binary.BigEndian.Uint16(unmarkedbuf))
     case mark32Bits:
         nbytes = 4
         if len(buf) < nbytes {
             err = ErrMalformedPayloadLength
             return
         }
-        buf[0] &= ^uint8(mark32Bits)
-        length = uint64(binary.BigEndian.Uint32(buf))
+        unmarkedbuf := append([]byte{}, buf...)
+        unmarkedbuf[0] &= ^uint8(mark32Bits)
+        length = uint64(binary.BigEndian.Uint32(unmarkedbuf))
     case mark64Bits:
         nbytes = 8
         if len(buf) < nbytes {
             err = ErrMalformedPayloadLength
             return
         }
-        buf[0] &= ^uint8(mark64Bits)
-        length = uint64(binary.BigEndian.Uint64(buf))
+        unmarkedbuf := append([]byte{}, buf...)
+        unmarkedbuf[0] &= ^uint8(mark64Bits)
+        length = uint64(binary.BigEndian.Uint64(unmarkedbuf))
     default:
         err = ErrMalformedPayloadLength
         return
--- payloadlength/payload_length_test.go
+++ payloadlength/payload_length_test.go
@@ -4,7 +4,34 @@ import (
     "testing"
 )
 
-func TestPayloadlength(t *testing.T) {
+func TestPayloadlength16(t *testing.T) {
+    length := uint16(0)
+    for ; length < max16Bits + 1; length++ {
+        cnb, err := CountBytes16(length)
+        if err != nil {
+            t.Errorf("CountBytes16 failed: %s", err)
+        }
+        buf, enb, err := Encode16(length)
+        if err != nil {
+            t.Errorf("Encode16 failed: %s", err)
+        }
+        lengthdec, dnb, err := Decode16(buf)
+        if err != nil {
+            t.Errorf("Decode16 failed: %s", err)
+        }
+        if length != lengthdec {
+            t.Errorf("%d != %d", length, lengthdec)
+        }
+        if cnb != enb {
+            t.Errorf("cnb != enb")
+        }
+        if enb != dnb {
+            t.Errorf("enb != dnb")
+        }
+    }
+}
+
+func TestPayloadlength64(t *testing.T) {
     length := uint64(0)
     for ; length < max64Bits + 1; length++ {
         cnb, err := CountBytes64(length)
